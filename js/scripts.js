e4eb8fb06f3f5dfda5dfdae13c5b131dde980b8f
Kevin Hölscher <kevin-hoelscher@gmx.de>
2023-08-18 03:32:11 +0200

    comments

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,18 @@ $(document).ready(function () {
   function newItem() {
     let inputValue = $("#input").val();
 
+    // some user feedback and adding the text value to the new created li
     if (inputValue === "") {
       alert("You must write something!");
     } else {
       let li = $("<li></li>").text(inputValue);
 
+      // crossing out handler
       li.on("dblclick", function () {
         $(this).toggleClass("strike");
       });
 
+      // delete handler
       let crossOutButton = $("<button>X</button>");
       crossOutButton.on("click", function () {
         $(this).parent().addClass("delete");
@@ -19,11 +22,13 @@ $(document).ready(function () {
 
       $("#list").append(li);
 
+      // clearing the input field
       $("#input").val("");
     }
   }
 
   $("#button").on("click", newItem);
 
+  // Sorting the Items
   $("#list").sortable();
 });
